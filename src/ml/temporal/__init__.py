71b2b87b57c76f248edf2655f4ac2afcdd3cd253
Eduardo Moraes <199427299+edu-moraess@users.noreply.github.com>
2026-09-01 20:50:49 -0300

    feat: export adaptive temporal scale selector

--- src/ml/temporal/__init__.py
+++ src/ml/temporal/__init__.py
@@ -1,7 +1,7 @@
 """Temporal deep-learning path for real AIS trajectories."""
 from src.ml.temporal.adapter import TemporalAnomalyAdapter
 from src.ml.temporal.benchmark import BenchmarkResult, compare_if_vs_deep, compare_snapshot, if_scores_from_embeddings
-from src.ml.temporal.diagnostics import TemporalTrackDiagnostics, analyze_temporal_tracks
+from src.ml.temporal.diagnostics import ADAPTIVE_SEQUENCE_LENGTHS, TemporalTrackDiagnostics, analyze_temporal_tracks, select_adaptive_sequence_length
 from src.ml.temporal.inference import InferenceResult, score_sequences
 from src.ml.temporal.model import GRUTemporalAutoencoder, TCNAutoencoder, TemporalResidualBlock, torch_available
 from src.ml.temporal.preprocess import FEATURE_DIM, FEATURE_NAMES, TemporalSequenceScaler, build_temporal_sequence, build_temporal_sequences, sequences_to_batch
@@ -12,7 +12,7 @@ __all__ = [
     "TemporalAnomalyAdapter", "TCNAutoencoder", "TemporalResidualBlock", "GRUTemporalAutoencoder", "torch_available",
     "score_sequences", "InferenceResult", "compare_snapshot", "compare_if_vs_deep", "if_scores_from_embeddings", "BenchmarkResult",
     "TemporalFitResult", "TemporalScore", "TemporalSequence", "TemporalSequenceScaler", "TemporalTrainer", "TrainingConfig", "TrainingResult",
-    "TemporalTrackDiagnostics", "analyze_temporal_tracks",
+    "TemporalTrackDiagnostics", "analyze_temporal_tracks", "select_adaptive_sequence_length", "ADAPTIVE_SEQUENCE_LENGTHS",
     "build_temporal_sequence", "build_temporal_sequences", "sequences_to_batch", "FEATURE_DIM", "FEATURE_NAMES", "TEMPORAL_FEATURE_NAMES",
     "MINIMUM_TRACKS_FOR_DEEP_MODEL", "MINIMUM_POINTS_PER_TRACK", "DEFAULT_SEQUENCE_LENGTH", "VALID_TEMPORAL_STATUSES",
 ]
